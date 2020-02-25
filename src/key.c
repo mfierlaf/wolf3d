9975a8a204e46d84b6174da56bed01827feba81d
mfierlaf <43782114+mfierlaf@users.noreply.github.com>
2020-02-25 17:50:10 +0100

    Add files via upload

--- src/key.c
+++ src/key.c
@@ -29,26 +29,26 @@ int				key_press(int key2, t_mlx *mlx)
 		free(mlx);
 		exit(-1);
 	}
-	if (key2 == W_KEY)
+	if (key2 == UP_KEY || key2 == W_KEY)
 		mlx->wkey = 1;
-	if (key2 == A_KEY)
+	if (key2 == LEFT_KEY || key2 == A_KEY)
 		mlx->akey = 1;
-	if (key2 == S_KEY)
+	if (key2 == DOWN_KEY || key2 == S_KEY)
 		mlx->skey = 1;
-	if (key2 == D_KEY)
+	if (key2 == RIGHT_KEY || key2 == D_KEY)
 		mlx->dkey = 1;
 	return (0);
 }
 
 int				key_release(int key2, t_mlx *mlx)
 {
-	if (key2 == W_KEY)
+	if (key2 == UP_KEY || key2 == W_KEY)
 		mlx->wkey = 0;
-	if (key2 == A_KEY)
+	if (key2 == LEFT_KEY || key2 == A_KEY)
 		mlx->akey = 0;
-	if (key2 == S_KEY)
+	if (key2 == DOWN_KEY || key2 == S_KEY)
 		mlx->skey = 0;
-	if (key2 == D_KEY)
+	if (key2 == RIGHT_KEY || key2 == D_KEY)
 		mlx->dkey = 0;
 	return (0);
 }
